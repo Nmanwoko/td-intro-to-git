1dfdc1f6faa28f436b08fb2e8b959b9043ccd169
Nmanwoko <87538412+Nmanwoko@users.noreply.github.com>
2021-08-27 11:56:10 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+
+namespace td_intro_to_git.Models;
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
